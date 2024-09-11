a3e613fa20d5967161ade4d6dbb418f0e7c15388
saheedoluwatosin <saheedoluwatosin4@gmail.com>
2024-09-11 17:18:08 +0100

    Updated

--- validateToken.js
+++ validateToken.js
@@ -11,13 +11,15 @@ const validtoken = async (request,response,next)=>{
 
         const decoded = jwt.verify(token1,process.env.ACCESS_URL)
         console.log(decoded)
-        const user = await User.findOne({_id:decoded._id})
-        request.user = user 
+        const user = await User.findOne({_id:decoded.user_login._id})
+        console.log(user)
+        
         if(!decoded){
             return response.status(401).json({
                 message:"Invalid Login"
             })
         }
+        request.user = user  
         next()
     } catch (error) {
         return response.status(500).json({error:error.message})
